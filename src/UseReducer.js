472cdff89df87cf4ea6eea7da7c2f10a9d51fdaf
Francisco Javier Rojas M <javier32rojas040506@gmail.com>
2022-08-23 21:01:16 -0500

    Final feature

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -8,9 +8,20 @@ const initialState = {
     confirm: false
 };
 
+
 function UseReducer(){
     const SECURITY_CODE = "paradigma";
     const [state, dispatch] = React.useReducer(reducer, initialState);
+    const OnError = () => dispatch({type: actionTypes.ERROR});
+    const Onconfirm = () => dispatch({type: actionTypes.CONFIRM});
+    const OnCheck = () => dispatch({type: actionTypes.CHECK});
+    const OnDelete = () => dispatch({type: actionTypes.DELETE});
+    const OnReset = () => dispatch({type: actionTypes.RESET});
+
+    const OnWrite = (event) => {
+        dispatch({type: actionTypes.WRITE, payload: event.target.value});
+    };
+
     React.useEffect(() => {
         console.log("Hook Searching State");
 
@@ -19,9 +30,9 @@ function UseReducer(){
                 console.log("Loading state...");
                 
                 if(state.inputValue !== SECURITY_CODE){
-                    dispatch({type: 'ERROR'});
+                    OnError();
                 }else{
-                   dispatch({type: 'CONFIRM'});
+                   Onconfirm();
                 }
                 console.log(state);
                 console.log("Loading state DONE");
@@ -37,10 +48,8 @@ function UseReducer(){
                 {state.error && <p>Error: El codigo es incorrecto</p>}
                 {state.loading && <p>Loading...</p>}
                 <input type='text' placeholder='Codigos de seguridad' value={state.inputValue} 
-                onChange={ event =>  dispatch({type:'WRITE', payload: event.target.value})}/>
-                <button onClick={ () => {
-                    dispatch({type: 'CHECK'}) 
-                }}>
+                onChange={ event =>  OnWrite(event)}/>
+                <button onClick={OnCheck}>
                     Comprobar
                 </button> 
             </div>
@@ -49,19 +58,28 @@ function UseReducer(){
         return (<div>
                    <h2>Eliminar UseReducer</h2> 
                    <p>¿Desea Eliminar el UseReducer</p>
-                   <button onClick={() => dispatch({type:'DELETE'})}>si, Borrar</button>
-                   <button onClick={() => dispatch({type: 'RESET'})}>No, Volver</button>
+                   <button onClick={OnDelete}>si, Borrar</button>
+                   <button onClick={OnReset}>No, Volver</button>
                 </div>);
     }else{
         return (
         <div>
             <h2>UseReducer Fue Eliminado</h2> 
-            <button onClick={() => dispatch({type: 'RESET'})}>Recuperar UseReducer</button>
+            <button onClick={OnReset}>Recuperar UseReducer</button>
          </div>
          );
     }
     
 }
+
+const actionTypes = {
+    ERROR: 'ERROR', 
+    CONFIRM:'CONFIRM', 
+    DELETE: 'DELETE',
+    WRITE:'WRITE', 
+    CHECK:'CHECK',
+    RESET:'RESET'
+};
 
 //with Object
 const reducerObject = (state, action) => ({
